abd7294cb1497912ffa24c3c28f03235121e5dea
himanshu kumar <himanshushubham555@gmail.com>
2021-12-01 08:09:22 +0530

    Remove unnecesssary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('clint side javascript file is loadded')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
